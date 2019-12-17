d3d21a84212909960ca3f48d6f25f2b701389cbc
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-12-16 22:01:08 -0700

    Level Created
    Level Complete

---
+++ SneakyMSF/Assets/Scripts/Enemy2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Enemy2 : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float distance;
+    public LineRenderer lineOfSight;
+    public Gradient redColor;
+    public Gradient greenColor;
+    public Gradient yellowColor;
+
+    //public Enemy other;
+    void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            lineOfSight.SetPosition(1, hitInfo.point);
+            lineOfSight.colorGradient = yellowColor;
+
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                lineOfSight.colorGradient = redColor;
+                Debug.Log("hit player");
+            }
+        }
+
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+            lineOfSight.SetPosition(1, transform.position + transform.right * distance);
+            lineOfSight.colorGradient = greenColor;
+        }
+
+        lineOfSight.SetPosition(0, transform.position);
+    }
+
+}
---
+++ SneakyMSF/Assets/Scripts/Movement1.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement1 : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetAnimationState();
+    }
+
+    void SetAnimationState()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isWalkingFront", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetBool("isWalkingFront", false);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalkingBack", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("isWalkingBack", false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalkingLeft", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool("isWalkingLeft", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalkingRight", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("isWalkingRight", false);
+        }
+    }
+}
